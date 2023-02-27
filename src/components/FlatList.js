9a1782ab71933c5355c4138de7053ea6b5d82ddc
heloisabett <heloisabettramosifc@gmail.com>
2023-02-27 15:04:14 -0300

    atividadereactnative

--- src/components/FlatList.js
+++ src/components/FlatList.js
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './src/components/Header';
 export default function App() {
+  const tasks = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3', 'Tarefa 4', 'Tarefa 5'];
   return (
     <View style={styles.container}>
       <Header />
-      <Text>Lista de tarefas</Text>
+      <View>
+        <FlatList data={tasks} renderItem={({ item }) => <Text>{item}</Text>} />
+      </View>
     </View>
   );
 }
